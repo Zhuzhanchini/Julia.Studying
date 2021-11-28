c50f24c6e58a180cd81313f4d977facb22c37646
Zhuzhanchini <asot2006@gmail.com>
2021-11-28 23:38:01 +0300

    hw1

--- app/src/main/java/example/julia/myapplication/MainActivity.kt
+++ app/src/main/java/example/julia/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package example.julia.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val helloTextView : TextView = findViewById(R.id.hellow_world_text_view)
+        val helloTextView: TextView = findViewById(R.id.hellow_world_text_view)
+        val newTextButton: Button = findViewById(R.id.new_text_button)
+
+        newTextButton.setOnClickListener {
+            helloTextView.text = "Hello"
+        }
 
 
     }
