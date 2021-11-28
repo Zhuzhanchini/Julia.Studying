12b7792d8e183dedfaf54ee7cccb125df9162d3b
Zhuzhanchini <asot2006@gmail.com>
2021-11-28 21:04:30 +0300

    hw1

---
+++ app/src/main/java/example/julia/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package example.julia.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val helloTextView : TextView = findViewById(R.id.hellow_world_text_view)
+        helloTextView.text = "Hello my name is Julia!"
+    }
+}
