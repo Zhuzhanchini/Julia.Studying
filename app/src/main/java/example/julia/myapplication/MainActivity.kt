1ab187157fc5133ce02d712c285501770621067a
Zhuzhanchini <asot2006@gmail.com>
2021-12-05 21:10:51 +0300

    hw7 - дизайн

--- app/src/main/java/example/julia/myapplication/MainActivity.kt
+++ app/src/main/java/example/julia/myapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package example.julia.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,20 +9,28 @@ import android.widget.Button
 import android.widget.TextView
 import kotlin.random.Random
 
+private const val HELLO_KEY = "hello"
+
 class MainActivity : AppCompatActivity() {
 
-    lateinit var randomTextView: TextView
-    lateinit var randomButton: Button
+    lateinit var nextActivityButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        nextActivityButton = findViewById(R.id.next_activity_button)
+
+        nextActivityButton.setOnClickListener {
+            val link = Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.youtube.kids&hl=ru&gl=US")
+            val openBrowserIntent = Intent(Intent.ACTION_VIEW, link)
+            val chooser=Intent.createChooser(openBrowserIntent, "PlayMarket")
+            startActivity(chooser)
+        }
+
+
 
     }
-
-
-
 
 
 }
--- app/src/main/java/example/julia/myapplication/SecondActivity.kt
+++ app/src/main/java/example/julia/myapplication/SecondActivity.kt
@@ -2,10 +2,21 @@ package example.julia.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
+private const val HELLO_KEY = "hello"
 
 class SecondActivity : AppCompatActivity() {
+
+    lateinit var helloTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        helloTextView = findViewById(R.id.hello_text_view)
+
+        val helloValue: String? = intent.extras?.getString(HELLO_KEY, "hello")
+        helloTextView.text = helloValue
     }
 }
